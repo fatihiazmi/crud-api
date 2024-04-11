603cdc9b1a38842fe0c607f63a4d27576f6fa1c9
fatihiazmi <fatihiazmi@gmail.com>
2024-04-11 08:38:45 +0800

    - front page added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <h1 class="text-2xl">Thanks for this opportunity</h1>
+
+    <ol class="mt-5">
+        <li class="my-2 text-xl">1. Clone this repository by running this command 'git clone https://github.com/fatihiazmi/crud-api.git'.</li>
+        <li class="my-2 text-xl">2. Install dependencies using 'composer install' command.</li>
+        <li class="my-2 text-xl">3. Run 'php artisan migrate --seed'.</li>
+        <li class="my-2 text-xl">4. Run 'php artisan serve' to serve the app locally.</li>
+        <li class="my-2 text-xl">5. (Optional) Visit 'http://localhost:8080/' to confirm the app is running</li>
+        <li class="my-2 text-xl">6. Open HTTP Client of your choice (Postman/RapidAPI).</li>
+        <li class="my-2 text-xl">7. Task 1 - Do a GET request on 'http://localhost:8080/api/users' to get all users.</li>
+        <li class="my-2 text-xl">8. Task 2 - Do a GET request on 'http://localhost:8080/api/users/{id}' to get a single user.</li>
+        <li class="my-2 text-xl">9. Task 3 - Do a POST request on 'http://localhost:8080/api/users' to create a user.</li>
+        <li class="my-2 text-xl">Task 3 (cont.) - Fill in the request body with these parameter <strong>name, email, password, password_confirmation</strong>.</li>
+        <li class="my-2 text-xl">10. Please note to <strong>copy the returned Bearer token</strong> after you create the user to be able <strong>to update and delete a user</strong>.</li>
+        <li class="my-2 text-xl">11. Paste the <strong>Bearer token into Auth > Bearer field</strong>.</li>
+        <li class="my-2 text-xl">12. Task 4 - Do a PUT request on 'http://localhost:8080/api/users' to edit a user.</li>
+        <li class="my-2 text-xl">13. Task 5 - Do a DELETE request on 'http://localhost:8080/api/users' to delete a user.</li>
+
+    </ol>
+</x-layout>
